a089ac8d80ed6eb357e4c282136b41e4ffd256b3
Manos <lionakisman@gmail.com>
2023-11-03 11:56:37 +0200

    hello

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+# Create a PyQt application instance
+app = QApplication(sys.argv)
+
+# Create a main window
+window = QWidget()
+window.setWindowTitle('Simple PyQt Window')
+
+
+label = QLabel('Hello, PyQt!')
+button = QPushButton('Click Me')
+
+
+layout = QVBoxLayout()
+layout.addWidget(label)
+layout.addWidget(button)
+
+
+window.setLayout(layout)
+
+
+def on_button_click():
+    label.setText('Button Clicked!')
+
+
+button.clicked.connect(on_button_click)
+
+window.show()
+
+sys.exit(app.exec_())
