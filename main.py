0acd9f00392b2d078c66f27cdb3095a3bda507c8
Manos <lionakisman@gmail.com>
2023-11-12 22:33:54 +0200

    First check!

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
-
-# Create a PyQt application instance
-app = QApplication(sys.argv)
-
-# Create a main window
-window = QWidget()
-window.setWindowTitle('GreenPY')
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QAction, QDesktopWidget, QFileDialog, \
+    QMessageBox
 
 
-label = QLabel('Hello, PyQt!')
-button = QPushButton('Click Me')
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Set window size
+        self.resize(800, 600)  # (x, y, width, height)
+        self.setWindowTitle('My PyQt5 Window')
+        self.center()
+
+        # Create menu bar
+        menubar = self.menuBar()
+
+        # Create File menu
+        file_menu = menubar.addMenu('File')
+
+        #File menu Open button
+        open_action = QAction('Open', self)
+        file_menu.addAction(open_action)
+        open_action.triggered.connect(self.open_file_dialog)
 
 
-layout = QVBoxLayout()
-layout.addWidget(label)
-layout.addWidget(button)
+        #Exit menu button
+        exit_action=QAction('Exit',self)
+        file_menu.addAction(exit_action)
+        exit_action.triggered.connect(self.close)
+    def center(self):
+        # Center the window on the screen
+        screen_geometry = QDesktopWidget().screenGeometry()
+        x = (screen_geometry.width() - self.width()) // 2
+        y = (screen_geometry.height() - self.height()) // 2
+        self.move(x, y)
+
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open File", "",
+                                                   "All Files (*);;Text Files (*.txt);;CSV Files (*.csv);;Python Files (*.py)",
+                                                   options=options)
+        if file_name:
+            print(f"Selected file: {file_name}")
+
+            if file_name[-3:] != '.py':
+                QMessageBox.warning(self, 'Warning', 'Invalid file type selected. Please choose a valid file type.')
+                return None
+            return file_name
 
 
-window.setLayout(layout)
 
-
-def on_button_click():
-    label.setText('Button Clicked!')
-
-
-button.clicked.connect(on_button_click)
-
-window.show()
-
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
