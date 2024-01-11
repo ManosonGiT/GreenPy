ff53f7ad52ddff6a5bc882f53a3cc22df73e22c7
Baramount <149668134+Baramount@users.noreply.github.com>
2024-01-11 13:47:57 +0100

    Update new.backend.py

--- new.backend.py
+++ new.backend.py
@@ -1,23 +1,60 @@
 import sys
+import psutil
 import ast
+import time
 import graphviz
-from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QMessageBox, QSplitter, QToolBar, QPushButton,
-    QTextEdit, QDialog, QVBoxLayout, QTextBrowser, QGraphicsView,
-    QGraphicsScene, QGraphicsPixmapItem, QAction
-)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QMessageBox, QSplitter, QToolBar, QPushButton, QTextEdit, QDialog
+,QVBoxLayout, QTextBrowser, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QAction)
 from PyQt5.QtGui import QIcon, QPixmap, QTextCursor
 from PyQt5.QtCore import QFile, QTextStream, Qt, QByteArray, QSize
 
-def analyze_code(code):
+def notify(message):
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Information)
+    msg_box.setText(message)
+    msg_box.setWindowTitle("Notification")
+    msg_box.exec_()
+
+# CO2 calculation
+def measure_cpu(code, country, processor):
+    try:
+        def user_code():
+            nonlocal code
+            exec(code)
+        
+        start_time = time.time()
+        initial_cpu_usage = psutil.cpu_percent(interval=None)
+        
+        user_code()
+        end_time = time.time()
+        final_cpu_usage = psutil.cpu_percent(interval=None)
+        cpu_usage_during_execution = final_cpu_usage - initial_cpu_usage
+        execution_time = end_time - start_time
+        
+         # calcul of CO2 eq
+        multiplier_constant = 1 / (1000 * 3600)
+        co2_eq = (cpu_usage_during_execution *processor *execution_time *country *multiplier_constant )
+        co2_eq = "{:.3}".format(co2_eq)
+        print(f"CPU Using : {cpu_usage_during_execution}%")
+        print(f"CO2 equivalent :{co2_eq}g CO2")
+        
+        # notif CO2
+        notify(f"CO2 equivalent : {co2_eq}g CO2")
+
+    except Exception as e:
+        notify(f"Error measuring CPU usage: {str(e)}")
+
+def analyze_code(code, country, processor):
     try:
         tree = ast.parse(code)
         dot = generate_dot(tree)
-        results = analyze_ast(tree)
-        show_results(dot, results)
+        while_statements = analyze_ast(tree)
+
+        show_results(dot, while_statements)
+        measure_cpu(code, country, processor)
     except Exception as e:
         notify(f"Error analyzing code: {str(e)}")
-        
+
 def analyze_ast(tree):
     while_statements = []
     for node in ast.walk(tree):
@@ -47,10 +84,11 @@ def generate_dot(tree):
 
 def show_results(dot, results):
     app = QApplication.instance()
-    dialog = ASTViewer()  # Supprimer la ligne existante d'instanciation de ASTViewer
-    dialog.dot = dot  # Assigner dot à l'attribut dot de ASTViewer
-    dialog.results = results  # Assigner results à l'attribut results de ASTViewer
+    dialog = ASTViewer()
+    dialog.dot = dot
+    dialog.results = results
     dialog.resize(800, 600)
+    dialog.show_while_result()
     dialog.exec_()
 
 def notify(message):
@@ -66,8 +104,6 @@ class ASTViewer(QDialog):
         self.setWindowTitle("AST Analysis Results")
         self.setWindowIcon(QIcon("GreenPy.png"))
         layout = QVBoxLayout()
-
-        
         
         self.see_ast_button = QPushButton('See AST')
         self.apply_stylesheet_to_button(self.see_ast_button, "style.css")
@@ -85,6 +121,7 @@ class ASTViewer(QDialog):
         self.setLayout(layout)
         self.dot = None
         self.results = None
+        self.tree_shown = False 
 
     def apply_stylesheet_to_button(self, button, style_path):
         style_file = QFile(style_path)
@@ -97,18 +134,24 @@ class ASTViewer(QDialog):
         button.setStyleSheet(stylesheet)
         style_file.close()
 
-    def show_ast_tree(self):
-        self.text_browser.setPlainText("\n".join(self.results))
+    def show_while_result(self):
+        while_statements = "\n".join(self.results)
+        self.text_browser.setPlainText(while_statements)
         self.text_browser.moveCursor(QTextCursor.End)
         self.text_browser.ensureCursorVisible()
-        if self.dot is not None and self.dot is not None:
+
+    def show_ast_tree(self):
+        if not self.tree_shown:  
+            if self.dot is not None:
             
-            pixmap = QPixmap()
-            pixmap.loadFromData(QByteArray(self.dot.pipe(format='png')), "PNG")
-            item = QGraphicsPixmapItem(pixmap)
-            self.scene.clear()
-            self.scene.addItem(item)
-            self.view.fitInView(item, Qt.KeepAspectRatio)
+                pixmap = QPixmap()
+                pixmap.loadFromData(QByteArray(self.dot.pipe(format='png')), "PNG")
+                item = QGraphicsPixmapItem(pixmap)
+                self.scene.clear()
+                self.scene.addItem(item)
+                self.view.fitInView(item, Qt.KeepAspectRatio)
+                
+                self.tree_shown = True  
 
     def wheelEvent(self, event):
         if event.modifiers() == Qt.ControlModifier:
@@ -143,12 +186,14 @@ class MyWindow(QMainWindow):
         self.move(x, y)
 
     def analyze_code(self):
-        code = self.editor.toPlainText()
-        analyze_code(code)
+        code = self.editor.toPlainText() 
+        try:
+            analyze_code(code)  
+        except Exception as e:
+            notify(f"Error analyzing code: {str(e)}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ast_viewer = ASTViewer()
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
